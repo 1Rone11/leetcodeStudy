4c56d901ee394997c29b9f4497d46ba093585e19
zchanglu <z18851923713@163.com>
2023-02-01 08:37:56 +0800

    🧪 test(2023/2/1 最小栈):

--- src/1.栈/12.[ 155 ] 最小栈.js
+++ src/1.栈/12.[ 155 ] 最小栈.js
@@ -3,12 +3,61 @@
  * * 题目地址：https://leetcode-cn.com/problems/min-stack
  */
 
-// * 思路：
+/**
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+    实现 MinStack 类:
+    MinStack() 初始化堆栈对象。
+    void push(int val) 将元素val推入堆栈。
+    void pop() 删除堆栈顶部的元素。
+    int top() 获取堆栈顶部的元素。
+    int getMin() 获取堆栈中的最小元素。
+ */
+// * 思路：定义两个栈，一个普通栈，一个最小栈，栈顶始终存放的是普通栈中的最小值
+var MinStack = function () {
+    this.stack = [];
+    this.minStack = [Infinity]
+};
 
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+    this.stack.push(val)
+    this.minStack.push(Math.min(this.minStack[this.minStack.length - 1], val))
+};
 
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+    this.stack.pop();
+    this.minStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+    return this.stack[this.stack.length - 1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+    return this.minStack[this.minStack.length - 1]
+};
 
 // 测试用例
-let test = ''
+MinStack()
+MinStack.prototype.push(-2)
+MinStack.prototype.push(0)
+MinStack.prototype.push(3)
+MinStack.prototype.getMin();
+MinStack.prototype.pop();
+MinStack.prototype.top();
+MinStack.prototype.getMin();
 
 console.time('执行用时');
 console.log(xxx(test));
