0237046506f3001098c7359451c9261e7a999682
zchanglu <z18851923713@163.com>
2023-02-22 08:50:55 +0800

    🧪 test(2023/2/22 字符串08):

--- src/3.字符串/08.[ 344 ] 反转字符串.js
+++ src/3.字符串/08.[ 344 ] 反转字符串.js
@@ -3,13 +3,28 @@
  * * 题目地址：https://leetcode-cn.com/problems/reverse-string
  */
 
-// * 思路：
-
+// * 思路：使用双指针left/right
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    const n = s.length
+    for (let left = 0, right = n - 1; left < right; left++, right--) {
+        [s[left], s[right]] = [s[right], s[left]] //解构赋值
+        //基本交换
+        // let temp;
+        // s[left] = temp;
+        // temp = s[right];
+        // s[right] = s[left]
+    }
+    return s
+};
 
 
 // 测试用例
-let test = ''
+let test = ["h", "e", "l", "l", "o"]
 
 console.time('执行用时');
-console.log(xxx(test));
+console.log(reverseString(test));
 console.timeEnd('执行用时');
