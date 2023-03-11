d3e953c596f2c4b6c9839a17e7d49a80f0abf5bc
zchanglu <z18851923713@163.com>
2023-03-11 16:04:46 +0800

    🧪 test(2023/3/11):

--- src/4.数组/04.[ 35 ] 搜索插入位置.js
+++ src/4.数组/04.[ 35 ] 搜索插入位置.js
@@ -1,15 +1,46 @@
+/*
+ * @Author: Zkiki
+ * @Date: 2023-01-02 15:57:04
+ * @LastEditTime: 2023-03-11 14:57:57
+ * @LastEditors: Please set LastEditors
+ * @FilePath: \leetcodeStudy\src\4.数组\04.[ 35 ] 搜索插入位置.js
+ * @Description: 
+ */
 /**
  * * 题目名称：搜索插入位置
  * * 题目地址：https://leetcode-cn.com/problems/search-insert-position
+ *   给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
  */
 
-// * 思路：
+// * 思路：该程序目的使找到第一个大于等于target的数组下标
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    const len = nums.length
+    //特殊情况
+    if (nums[len - 1] < target) return len
 
+    //到这里nums[len-1]一定有大于等于target的值
+    let left = 0;
+    let right = len - 1
+    while (left < right) {
+        const mid = left + parseInt((right - left) / 2) // 避免数值过大溢出
+        if (nums[mid] < target) {
+            left = mid + 1 // 范围是[mid + 1,right]
+        } else {
+            right = mid // 范围是[left,mid]
+        }
+    }
+    return left
+};
 
 
 // 测试用例
-let test = ''
+let test = [1, 3, 5, 6]
 
 console.time('执行用时');
-console.log(xxx(test));
+console.log(searchInsert(test, 5));
 console.timeEnd('执行用时');
--- src/4.数组/05.[ 面试题 10.01 ] 合并排序的数组.js
+++ src/4.数组/05.[ 面试题 10.01 ] 合并排序的数组.js
@@ -1,15 +1,69 @@
+/*
+ * @Author: Zkiki
+ * @Date: 2023-01-02 15:57:04
+ * @LastEditTime: 2023-03-11 15:34:22
+ * @LastEditors: Please set LastEditors
+ * @FilePath: \leetcodeStudy\src\4.数组\05.[ 面试题 10.01 ] 合并排序的数组.js
+ * @Description: 
+ */
 /**
  * * 题目名称：合并排序的数组
  * * 题目地址：https://leetcode-cn.com/problems/sorted-merge-lcci
+ * 给定两个排序后的数组 A 和 B，其中 A 的末端有足够的缓冲空间容纳 B。 编写一个方法，将 B 合并入 A 并排序。
+
+初始化 A 和 B 的元素数量分别为 m 和 n。
+
+示例:
+
+输入:
+A = [1,2,3,0,0,0], m = 3
+B = [2,5,6],       n = 3
+
+输出: [1,2,2,3,5,6]
  */
 
-// * 思路：
+// * 方法1：根据已排序的性质，使用双指针，指向两个数组，依次遍历取最小
+/**
+ * @param {number[]} A
+ * @param {number} m
+ * @param {number[]} B
+ * @param {number} n
+ * @return {void} Do not return anything, modify A in-place instead.
+ */
+var merge = function (A, m, B, n) {
+    let pa = 0;
+    let pb = 0;
+    const sorted = new Array(m + n).fill(0)
+    let cur;
+    while (pa < m || pb < n) {
+        if (pa === m) {
+            cur = B[pb++]
+        } else if (pb === n) {
+            cur = A[pa++]
+        } else if (A[pa] < B[pb]) {
+            cur = A[pa++]
+        } else {
+            cur = B[pb++]
+        }
+        sorted[pa + pb - 1] = cur
+    }
+    for (let i = 0; i != m + n; ++i) {
+        A[i] = sorted[i]
+    }
+};
 
-
+//方法2：先将两个数组合并，再排序
+const merge1 = (A, m, B, n) => {
+    A.splice(m, A.length - m, ...B)
+    A.sort((a, b) => a - b)
+    console.log(A);
+}
 
 // 测试用例
-let test = ''
+const A = [1, 2, 3, 0, 0, 0]
+const B = [2, 5, 6]
 
 console.time('执行用时');
-console.log(xxx(test));
+console.log(merge(A, 3, B, 3));
+// console.log(merge1(A, 3, B, 3));
 console.timeEnd('执行用时');
