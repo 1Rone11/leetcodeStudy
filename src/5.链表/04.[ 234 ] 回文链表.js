79e0d6a5db4078a3ce00c536316895924de39b28
zchanglu <z18851923713@163.com>
2023-10-20 13:49:00 +0800

    20231020 5链表4

--- src/5.链表/04.[ 234 ] 回文链表.js
+++ src/5.链表/04.[ 234 ] 回文链表.js
@@ -4,12 +4,46 @@
  */
 
 // * 思路：
+//链表定义
+function ListNode(val) {
+    this.val = val;
+    this.next = null
+}
 
+//创建链表
+function createLinkedList(arr) {
+    if (arr.length === 0) return null
+    let head = new ListNode(arr[0])
+    let cur = head
+    for (let i = 1; i < arr.length; i++) {
+        cur.next = new ListNode(arr[i])
+        cur = cur.next
+    }
+    return head
+}
 
+const testArr = createLinkedList([1, 2, 2, 1])
 
+// 回文链表
+const palindrome = (head) => {
+    // 1.遍历链表，获取链表对应数组
+    const vals = [];
+    while (head !== null) {
+        vals.push(head.val)
+        head = head.next
+    }
+
+    // 2.头尾指针判断回文
+    for (let i = 0, j = vals.length - 1; i < j; i++, j--) {
+        if (vals[i] !== vals[j]) {
+            return false
+        }
+    }
+    return true
+}
 // 测试用例
 let test = ''
 
 console.time('执行用时');
-console.log(xxx(test));
+console.log(palindrome(testArr));
 console.timeEnd('执行用时');
